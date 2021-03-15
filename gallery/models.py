b8f2f3ef1670b08aa3d10e62ae6b44c4dd56daff
Shvd <2100992@gmail.com>
2021-03-15 08:41:43 +0300

    development of an idea

--- gallery/models.py
+++ gallery/models.py
@@ -8,10 +8,20 @@ from app.models import BaseModel
 from slugify import slugify
 from datetime import datetime
 
+from sqlalchemy import exc
+
+BASE_COLLECTIONS_PATH = 'media/collections'
+
 collections_tags = db.Table(
-    'collectiond_tags',
+    'collections_tags',
     db.Column('collection_id', db.Integer, db.ForeignKey('collections.id')),
     db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'))
+)
+
+pictures_collections = db.Table(
+    'puctures_collections',
+    db.Column('collections_id', db.Integer, db.ForeignKey('collections.id')),
+    db.Column('pictures_id', db.Integer, db.ForeignKey('pictures.id'))
 )
 
 
@@ -20,57 +30,92 @@ class Collection(BaseModel):
 
     name = db.Column(db.String(140),
                      index=True,
-                     unique=True,
+                     #  unique=True,
                      nullable=False)
-
     path = db.Column(db.String(1024),
                      nullable=False)
-
     slug = db.Column(db.String(140),
                      index=True,
                      unique=True)
 
-    parrent_collection_id = db.Column(db.Integer,
-                                      db.ForeignKey('collections.id')
-                                      )
+    parrent_id = db.Column(db.Integer,
+                           db.ForeignKey('collections.id'))
+    parrent = db.relationship('Collection',
+                              backref=db.backref(
+                                  'children', lazy='dynamic'),
+                              remote_side='Collection.id')
 
-    parrent_collection = db.relationship('Collection',
-                                         backref=db.backref(
-                                             'children_collection', lazy='dynamic')
-                                         )
-
-    author_id = db.Column(db.Integer,
-                          db.ForeignKey('users.id'))
-
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     author = db.relationship('User',
                              backref=db.backref(
-                                 'collections', lazy='dynamic')
-                             )
+                                 'collections', lazy='dynamic'))
 
     tags = db.relationship('Tag',
                            secondary=collections_tags,
                            backref=db.backref(
-                               'collections', lazy='dynamic')
-                           )
+                               'collections', lazy='dynamic'))
 
-    # @property
-    # def url(self):
-    #     return url_for('gallery.set_details', slug=self.slug)
+    @property
+    def url(self, app=''):
+        if app:
+            return url_for(f'{app}.collection_details', slug=self.slug)
+        return url_for('gallery.collection_details', slug=self.slug, _external=True)
+
+    @property
+    def _path(self):
+        return os.path.join(app.static_folder, self.path)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def save(self, *args, **kwargs):
-        if not self.id:
-            self.path = os.path.join(
-                'media/collections', self.name)
-            full_path = os.path.join(
-                app.static_folder, 'media/collections', self.name)
-            if not os.path.isdir(full_path):
-                os.mkdir(full_path)
+        if not self.path:
+            if self.parrent:
+                base_path = self.parrent.path
+            else:
+                base_path = BASE_COLLECTIONS_PATH
+            self.path = os.path.join(base_path, self.name)
+
+        if not self.name:
+            self.name = os.path.split(self.path)[-1]
+
+        if not os.path.isdir(os.path.join(app.static_folder, self.path)):
+            os.makedirs(os.path.join(
+                app.static_folder, self.path), exist_ok=True)
+
         if not self.slug:
             self.generate_slug()
         super().save(*args, **kwargs)
+
+    def scan_dir(self):
+        filesystem_path = os.path.join(app.static_folder, self.path)
+        forbidden_path = []
+        stopfile = 'nomedia'
+        PICTURE_EXTS = ['.jpg', '.jpeg', '.png', '.bmp']
+
+        for root, dirs, files in os.walk(filesystem_path):
+            if root in forbidden_path:
+                print(f'forbidden_path - {root}')
+            elif stopfile in files:
+                print(f'nomedia dir - {root}')
+            else:
+                for file in files:
+                    ext = os.path.splitext(file)[1].lower()
+                    if ext in PICTURE_EXTS:
+                        _path = os.path.join(root, file)
+                        path = os.path.relpath(_path, app.static_folder)
+                        picture = Picture.query.filter_by(path=path).first()
+                        if not picture:
+                            picture = Picture(
+                                path=path,
+                            )
+                            picture.save()
+                        picture.collections.append(self)
+
+    def scan_db(self):
+        for picture in self.pictures:
+            if not os.path.isfile(picture._path):
+                picture.destroy()
 
     def generate_slug(self):
         time = datetime.now().strftime("%y%m%d%H%M%S")
@@ -80,7 +125,7 @@ class Collection(BaseModel):
             self.slug = slugify(f'No name -{time}')
 
     def __repr__(self):
-        return f'<Set {self.name}>'
+        return f'<Collection {self.name}>'
 
     def __str__(self):
         return self.__repr__()
@@ -93,11 +138,17 @@ class Picture(BaseModel):
     name = db.Column(db.String(140), index=True)
     collection_id = db.Column(db.Integer, db.ForeignKey('collections.id'))
 
-    collection = db.relationship('Collection',
-                                 backref=db.backref('pictures',
-                                                    lazy='dynamic'
-                                                    )
-                                 )
+    collections = db.relationship('Collection',
+                                  secondary=pictures_collections,
+                                  backref=db.backref('pictures',
+                                                     lazy='dynamic'
+                                                     )
+                                  )
+
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    author = db.relationship('User',
+                             backref=db.backref(
+                                 'pictures', lazy='dynamic'))
 
     def __init__(self, *args, path='', name='', **kwargs):
         self.path = path
@@ -108,4 +159,8 @@ class Picture(BaseModel):
 
     @property
     def url(self):
-        return url_for('static', filename=self.path)
+        return url_for('static', filename=self.path, _external=True)
+
+    @property
+    def _path(self):
+        return os.path.join(app.static_folder, self.path)
