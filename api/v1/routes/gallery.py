7f6484774ec25a60b98aac954a062a9fe2e96679
Shvd <2100992@gmail.com>
2021-03-15 14:17:38 +0300

    add picture_detals resuorce with similar_pictures

--- api/v1/routes/gallery.py
+++ api/v1/routes/gallery.py
@@ -2,13 +2,13 @@ from .. import api
 
 import urllib.parse
 
-from gallery.models import Collection, Picture
+from gallery.models import Collection, Picture, HammingDistace
 
 from flask import request, url_for
 
 from flask_restful import Resource, Api
 
-from ..serializers import CollectionsSchema, PictureSchema
+from ..serializers import CollectionsSchema, PictureSchema, SimilarPictureSchema
 
 
 class Collections(Resource):
@@ -56,28 +56,50 @@ class CollectionDetails(Resource):
 
         collection = Collection.query.filter_by(slug=slug).first_or_404()
         items = schema.dump(collection)
-        
+
         items['parrent'] = schema.dump(collection.parrent)
 
         if items['parrent']:
-            items['parrent']['url'] = urllib.parse.urljoin(url, items['parrent']['slug'])
+            items['parrent']['url'] = urllib.parse.urljoin(
+                url, items['parrent']['slug'])
 
-        items['children'] = [schema.dump(child) for child in collection.children.all()]
+        items['children'] = [schema.dump(child)
+                             for child in collection.children.all()]
         for child in items['children']:
             child['url'] = urllib.parse.urljoin(url, child['slug'])
 
-        pictures = [PictureSchema().dump(picture) for picture in collection.pictures.all()]
-
+        pictures = [PictureSchema().dump(picture)
+                    for picture in collection.pictures.all()]
 
         return {
             'collection': items,
             'pictures': pictures
         }
-        
 
     def post(self):
         pass
 
 
+class PictureDetails(Resource):
+    def get(self, id):
+        schema = PictureSchema()
+
+        picture = Picture.query.filter_by(id=id).first_or_404()
+        similar_pictures = picture.similar_pictures.union(
+            picture.similar_pictures_reverse).order_by(HammingDistace.distance).all()
+
+        return {
+            'picture': schema.dump(picture),
+            'similar_picture': [SimilarPictureSchema().dump(picture) for picture in similar_pictures]
+        }
+
+    def post(self, id):
+        pass
+
+
+api.add_resource(Collections, '/gallery/', endpoint='gallery')
 api.add_resource(Collections, '/gallery/collections/', endpoint='collections')
-api.add_resource(CollectionDetails, '/gallery/collections/<string:slug>/', endpoint='collection_details')
+api.add_resource(CollectionDetails, '/gallery/collections/<string:slug>/',
+                 endpoint='collection_details')
+api.add_resource(PictureDetails, '/gallery/pictures/<int:id>',
+                 endpoint='pictere_details')
